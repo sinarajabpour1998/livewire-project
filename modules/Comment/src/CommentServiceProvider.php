cf785c816625e1dbabec0a64e0d3aa791a5f2f11
Sina <sina.rajabpour@outlook.com>
2021-02-20 17:15:29 +0330

    First commit

---
+++ modules/Comment/src/CommentServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Comment;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+     public function boot()
+     {
+         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+         $this->loadViewsFrom(__DIR__ . '/views','comment');
+         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+         $this->mergeConfigFrom(__DIR__ . '/config/comment.php', 'comment');
+         $this->publishes([
+                __DIR__.'/config/comment.php' =>config_path('comment')
+         ], 'comment');
+         // This line is the menu initialize helper based on dizatech/module-menu package
+         //\ModuleMenu::init('comment');
+     }
+}
